106932dd31e4906b144166de4b62668e8add2efe
홍성은 <hongse21@gmail.com>
2021-01-26 23:26:08 +0900

    Update valid_palindrome using deque

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -1,18 +1,21 @@
-# 유효한 팰린드롬
-def isPalindrome(string):
+import collections
+# 유효한 팰린드롬, 데크 사용을 통해 최적화
 
-    strs = []
+
+def isPalindrome(string):
+    # 자료형을 데크로 선언
+    strs = collections.deque()
+
     for char in string:
         if char.isalnum():             # isalnum(): 문자, 숫자 판별 함수
             strs.append(char.lower())  # strs 리스트에 소문자로 변환하여 추가
 
-    while len(strs) > 1:               # 리스트에 들어간 값 palindrome 여부 판별
-        if strs.pop(0) != strs.pop():
+    while(len(strs)) > 1:
+        if strs.popleft() != strs.pop():
             return False
 
     return True
 
 
-# 테스트
 print(isPalindrome("A man, a plan, a canal: Panama"))
 print(isPalindrome("race a car"))
