d46ffdf3f05fda7b3ded6bd5bb63ff50e91c2314
홍성은 <hongse21@gmail.com>
2021-02-01 20:13:04 +0900

    Add twoSum2 usting 'in' search

--- two-sum.py
+++ two-sum.py
@@ -14,3 +14,16 @@ def twoSum(nums, target):
 
 print(twoSum([2, 7, 11, 15], 18))
 
+# 풀이 2.in 을 이용한 탐색
+
+
+def twoSumIn(nums, target):
+
+    for i, n in enumerate(nums):
+        complement = target - n
+
+        if complement in nums[i + 1:]:
+            return nums.index(n), nums[i + 1:].index(complement) + (i + 1)
+
+
+print(twoSum([2, 7, 11, 15], 18))
